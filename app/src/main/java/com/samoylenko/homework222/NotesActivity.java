b6c07d97766d6842b3c2fb05ce4fb7c186e55295
Alexandr Samoylenko <admin@room13.ru>
2020-05-22 12:45:35 +0700

    Initial commit AppBar

---
+++ app/src/main/java/com/samoylenko/homework222/NotesActivity.java
@@ -0,0 +1,62 @@
+package com.samoylenko.homework222;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class NotesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        myToolbar.setTitle(R.string.tittle_notes);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+            Intent intent = new Intent(getApplicationContext(), NotesActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, R.string.toast_notes, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id == R.id.action_main) {
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, R.string.toast_main, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id == R.id.action_open_calc) {
+            Intent intent = new Intent(getApplicationContext(), CalcActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, R.string.toast_calc, Toast.LENGTH_LONG).show();
+            return true;
+        }
+        if (id == R.id.action_pay) {
+            Intent intent = new Intent(getApplicationContext(), PayActivity.class);
+            startActivity(intent);
+            Toast.makeText(this, R.string.toast_pay, Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
